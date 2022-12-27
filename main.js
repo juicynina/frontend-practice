ab4699b0dca718a832c16e96125daf2c805263d2
Nina Herrera <ninaeherrera@gmail.com>
2022-12-27 12:22:46 -0500

    integré el shopping cart a index y ahora se pueden abrir y cerrar sin que choquen el desktop-menu, el mobile-menu y el shopping-cart

--- main.js
+++ main.js
@@ -2,14 +2,27 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuShoppingCart = document.querySelector('.navbar-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamburguesa.addEventListener('click', toggleMenuHamburguesa)
+menuShoppingCart.addEventListener('click', toggleShoppingCart)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive')
+    aside.classList.add('inactive')
 } 
 
 function toggleMenuHamburguesa() {
     mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive')
+    aside.classList.add('inactive')
 } 
+
+function toggleShoppingCart() {
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive')
+    mobileMenu.classList.add('inactive')
+}
