784b30d148f45b31398c0e2b48f540c8811594cc
Nina Herrera <ninaeherrera@gmail.com>
2022-12-28 15:21:10 -0500

    se pasó el html del product list para js con la creaciǿn de un array de todos los productos como objetos, y luego haciendo la funcion render products para poder usarla en el futuro

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuHamburguesa = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuShoppingCart = document.querySelector('.navbar-cart');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
 menuHamburguesa.addEventListener('click', toggleMenuHamburguesa)
@@ -26,3 +27,79 @@ function toggleShoppingCart() {
     desktopMenu.classList.add('inactive')
     mobileMenu.classList.add('inactive')
 }
+
+const productList = []
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/1595483/pexels-photo-1595483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+productList.push({
+    name: 'Screen',
+    price: 300,
+    image: 'https://images.pexels.com/photos/159394/pc-computer-android-android-pc-159394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+productList.push({
+    name: 'Headphones',
+    price: 90,
+    image: 'https://images.pexels.com/photos/610945/pexels-photo-610945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/1595483/pexels-photo-1595483.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="product-img">
+    <div class="product-info">
+        <div>
+            <p>$120.00</p>
+            <p>Bike</p>
+        </div>
+        <figure>
+            <img src="../icons/bt_add_to_cart.svg" alt="/">
+        </figure>
+    </div>
+</div>  */
+// product = {name, price, image} -> product.image (en vez de colocar el 
+        // link a la imagen que coloca como se buscaría en el array/objeto)
+
+function renderProducts(arr) {
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        // product = {name, price, image} -> product.image (en vez de colocar el 
+        // link a la imagen que coloca como se buscaría en el array/objeto)
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice)
+        productInfoDiv.appendChild(productName)
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productList);
+renderProducts(productList);
+renderProducts(productList);
